0b0142f34d3fab7169c54c02c3a2549779d93aaa
chengjunsen <chengjunsen@kingsoft.com>
2018-10-29 16:22:56 +0800

    index on master: 53d9d87 加入屏幕适配

---
+++ app/src/main/cpp/render/gl_watermark_render.cpp
@@ -0,0 +1,5 @@
+//
+// Created by chengjunsen on 2018/10/29.
+//
+
+#include "gl_watermark_render.h"
---
+++ app/src/main/cpp/render/gl_watermark_render.h
@@ -0,0 +1,16 @@
+//
+// Created by chengjunsen on 2018/10/29.
+//
+
+#ifndef SPLAYER_GL_WATERMARK_RENDER_H
+#define SPLAYER_GL_WATERMARK_RENDER_H
+
+
+
+class gl_watermark_render {
+
+};
+
+
+
+#endif //SPLAYER_GL_WATERMARK_RENDER_H
